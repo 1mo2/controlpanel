46c971bb652949b52f1d2c5a59a2cd8e5cb591a4
mo'men <mo9980716@gmail.com>
2023-08-03 06:20:09 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// import './assets/sass/style.scss';
+// // import '.normalize.css/normalize.css'
+
+// console.log("hello form webpack");
+
+
+import './assets/sass/style.scss';
+import 'normalize.css/normalize.css';
+
+console.log("hello from webpack");
